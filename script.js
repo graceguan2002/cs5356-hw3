e834eb101c9c15104d95ce17c5110b2835f02b31
graceguan2002 <guan22j@mtholyoke.edu>
2025-02-26 07:40:17 -0500

    Final version

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => {
-        const dogs = document.getElementById('dogs');
-        
-        data.message.forEach(imageUrl => {
-            const imgElement = document.createElement('img'); 
-            imgElement.src = imageUrl; 
-            imgElement.alt = 'Random Dog Photo';
-            dogs.appendChild(imgElement);
-          });
+        const dogImageElement = document.getElementById('dog-image');
+        dogImageElement.src = data.message; 
+        dogImageElement.alt = 'Random Dog Photo';
     })
     .catch(error => {
         console.error('Error fetching dog photo:', error);
